73050c1e4fa66194846dabaa2d254408560c48ae
Andy Ron <randy.njfu@gmail.com>
2020-02-15 20:39:56 +0800

    corejava v1ch05

---
+++ CoreJava/v1ch05/inheritance/src/inheritance/Manager.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+/**
+ * @author Andy Ron
+ */
+public class Manager extends Employee {
+    private double bonus;
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        return super.getSalary() + bonus;
+    }
+}
