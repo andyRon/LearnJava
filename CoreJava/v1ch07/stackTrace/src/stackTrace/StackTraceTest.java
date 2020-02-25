2cac60a088702cb8270531d056f0579b760e8d88
Andy Ron <randy.njfu@gmail.com>
2020-02-25 17:19:10 +0800

    Core Java v1 ch07

---
+++ CoreJava/v1ch07/stackTrace/src/stackTrace/StackTraceTest.java
@@ -0,0 +1,31 @@
+package stackTrace;
+
+import java.util.Scanner;
+
+/**
+ * 7.2.6 分析堆栈轨迹元素
+ * @author Andy Ron
+ */
+public class StackTraceTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = in.nextInt();
+        factorial(n);
+    }
+
+    public static int factorial(int n) {
+        System.out.println("factorial(" + n + "):");
+        Throwable t = new Throwable();
+        StackTraceElement[] frames = t.getStackTrace();
+        for (StackTraceElement f : frames)
+            System.out.println(f);
+
+        int r;
+        if (n <= 1) r = 1;
+        else r = n * factorial(n - 1);
+        System.out.println("return " + r);
+        return r;
+     }
+}
