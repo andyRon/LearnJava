5e0dfada48e70fcfde3d6bfe5a1c525d8e718787
Andy Ron <randy.njfu@gmail.com>
2020-02-25 17:18:39 +0800

    Core Java v1 ch06

---
+++ CoreJava/v1ch06/innerClass/src/InnerClassTest.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.*;
+
+/**
+ * @author Andy Ron
+ */
+public class InnerClassTest {
+
+    public static void main(String[] args) {
+        TalkingClock clock = new TalkingClock(1000, true);
+        clock.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+class TalkingClock {
+
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep) {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    public void start() {
+        ActionListener listener = new TimePrinter();
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+
+    class TimePrinter implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("At the tone, the time is " + new Date());
+            if (beep) Toolkit.getDefaultToolkit().beep();
+            // 获得默认工具箱。
+            // 发出一声铃声
+        }
+    }
+}
+
---
+++ CoreJava/v1ch06/lambda/src/LambdaTest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Date;
+import javax.swing.*;
+
+/**
+ * @author Andy Ron
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        String[] planets = new String[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Nepture"};
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted in dictionary order:");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted by length:");
+        Arrays.sort(planets, (first, second) -> first.length() - second.length());
+        System.out.println(Arrays.toString(planets));
+
+        Timer t = new Timer(1000, event -> System.out.println("The time is" + new Date()));
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
