9e043e9f56fbd8fd7a4143e3ef9bf5384a36b979
Andy Ron <randy.njfu@gmail.com>
2020-03-01 22:07:26 +0800

    Core Java v2 ch06

---
+++ CoreJava/v2ch06/timeline/src/TimeLine.java
@@ -0,0 +1,38 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * @author Andy Ron
+ */
+public class TimeLine {
+
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+        runAlgorithm();
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        long millis = timeElapsed.toMillis();
+        System.out.printf("%d milliseconds\n", millis);
+
+        Instant start2 = Instant.now();
+        runAlgorithm();
+        Instant end2 = Instant.now();
+        Duration timeElapsed2 = Duration.between(start2, end2);
+        System.out.printf("%d milliseconds\n", timeElapsed2.toMillis());
+        boolean overTenTimesFaster = timeElapsed.multipliedBy(10).minus(timeElapsed2).isNegative();
+        System.out.printf("The first algorithm is %smore than ten times faster",
+                overTenTimesFaster ? "" : "not ");
+    }
+
+    public static void runAlgorithm() {
+        int size = 10;
+        List<Integer> list = new Random().ints().map(i -> i % 100)
+                .limit(size).boxed().collect(Collectors.toList());
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
