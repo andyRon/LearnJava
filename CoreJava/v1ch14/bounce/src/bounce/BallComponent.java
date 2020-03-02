53f63697712776f52cd8c9802fb52c88dc1d431f
Andy Ron <randy.njfu@gmail.com>
2020-03-02 22:17:05 +0800

    core java update

---
+++ CoreJava/v1ch14/bounce/src/bounce/BallComponent.java
@@ -0,0 +1,35 @@
+package bounce;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * @author Andy Ron
+ */
+public class BallComponent extends JPanel {
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HEIGHT = 350;
+
+    private java.util.List<Ball> balls = new ArrayList<>();
+
+    public void add(Ball b) {
+        balls.add(b);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        for (Ball b : balls) {
+            g2.fill(b.getShape());
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+
+}
---
+++ CoreJava/v2ch05/view/src/view/ViewDB.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * @author Andy Ron
+ */
+public class ViewDB {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ViewDBFrame extends JFrame {
+
+}
