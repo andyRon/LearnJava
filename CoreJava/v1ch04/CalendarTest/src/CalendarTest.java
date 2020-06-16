7d9332d3b7f429bb509444524ecd7355f2af83df
Andy Ron <randy.njfu@gmail.com>
2020-06-16 23:40:41 +0800

    《Java核心技术卷一》

--- CoreJava/v1ch04/CalendarTest/src/CalendarTest.java
+++ CoreJava/v1ch04/CalendarTest/src/CalendarTest.java
@@ -11,24 +11,32 @@ public class CalendarTest {
         int month = date.getMonthValue();
         int today = date.getDayOfMonth();
 
-        date = date.minusDays(today - 1);   // 把date设置成当前月的第一天
+        // 把date设置成当前月的第一天
+        date = date.minusDays(today - 1);
         DayOfWeek weekday = date.getDayOfWeek();
         int value = weekday.getValue();
 
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-        for (int i = 1; i < value; i++)
-            System.out.print("    ");  // 四个空格，表示如"Mon "
+        for (int i = 1; i < value; i++) {
+            // 四个空格，表示如"Mon "
+            System.out.print("    ");
+        }
 
         while (date.getMonthValue() == month) {
             System.out.printf("%3d", date.getDayOfMonth());
-            if (date.getDayOfMonth() == today)
+            if (date.getDayOfMonth() == today) {
                 System.out.print("*");
-            else
+            } else {
                 System.out.print(" ");
+            }
             date = date.plusDays(1);
-            if (date.getDayOfWeek().getValue() == 1) System.out.println();
+            if (date.getDayOfWeek().getValue() == 1) {
+                System.out.println();
+            }
         }
-        if (date.getDayOfWeek().getValue() != 1) System.out.println();
+        if (date.getDayOfWeek().getValue() != 1) {
+            System.out.println();
+        }
     }
 
 }
