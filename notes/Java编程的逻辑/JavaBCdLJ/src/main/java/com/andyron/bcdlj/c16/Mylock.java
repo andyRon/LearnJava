bac59aa067dda0657cbba9b1b8f93d9e8f910de7
Andy Ron <randy.njfu@gmail.com>
2022-07-04 16:53:11 +0800

    Java编程的逻辑

---
+++ notes/Java编程的逻辑/JavaBCdLJ/src/main/java/com/andyron/bcdlj/c16/Mylock.java
@@ -0,0 +1,21 @@
+package com.andyron.bcdlj.c16;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 使用AtomicInteger实现锁
+ * status表示锁的状态，0表示未锁定，1表示锁定，lock()、unlock()使用CAS方法更新，lock()只有在更新成功后才退出，实现了阻塞的效果
+ */
+public class Mylock {
+    private AtomicInteger status = new AtomicInteger(0);
+
+    public void lock() {
+        while (!status.compareAndSet(0, 1)) {
+            Thread.yield();
+        }
+    }
+
+    public void unlock() {
+        status.compareAndSet(1, 0);
+    }
+}
